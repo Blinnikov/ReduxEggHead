8a4ff774584e89b34297153c634dabed086643fb
Igor Blinnikov <igor.blinnikov@gmail.com>
2016-12-11 21:45:08 +0300

    Add React Redux
    Use Provider from react-redux
    Remove separate store file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Provider from './Provider';
+import { Provider } from 'react-redux';
 import TodoApp from './TodoApp'
 import { createStore } from 'redux';
 import todoApp from './reducers/todo-app';
--- src/store.js
+++
@@ -1,5 +0,0 @@
-import { createStore } from 'redux';
-import todoApp from './reducers/todo-app';
-
-const store = createStore(todoApp);
-export default store;
